76c54c97e31da689db51e67e1125443652d5603b
singh-vsc <vsingh@stegtechnologies.com>
2018-08-20 16:27:49 +0530

    deals message

--- scripts/modules/confirmation.js
+++ scripts/modules/confirmation.js
@@ -68,7 +68,7 @@ require(["modules/jquery-mozu", "underscore", 'modules/api', "hyprlive", "module
                 var email = $('#emailId').val();
                 if(deals !== '') {
                     api.request("POST", "/mailchimp", {'accountId':email, 'deals':deals}).then(function (response){
-                       console.log("Response 22 : "+JSON.stringify(response));    
+                       $(".mz-look-email").show().delay(5000).fadeOut();  
                     }, function(err) {
                         console.log("Failure : "+JSON.stringify(err));
                     });
