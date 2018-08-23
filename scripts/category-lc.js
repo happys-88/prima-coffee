008a4d8d91d67e40f8e7056e1c3270c3ad55953d
Happy <hmasih@stegtechnologies.com>
2018-08-23 15:54:50 +0530

    Code Review Changes - UI

--- scripts/category-lc.js
+++ scripts/category-lc.js
@@ -1,13 +1,11 @@
 require([
 	'modules/jquery-mozu',
-	"modules/views-collections",
-	"hyprlivecontext"
-], function( $, CollectionViewFactory, HyprLiveContext) { 
+	"modules/views-collections"
+], function( $, CollectionViewFactory) { 
     $(document).ready(function() {
     	window.facetingViewsLC = CollectionViewFactory.createFacetedCollectionViews({  
             $body: $('#lcCategoryContent'),   
             template: "modules/learning-center/category-interior-lc" 
         });
-		
-	}); 
+	});  
 }); 
--- scripts/pages/category.js
+++ scripts/pages/category.js
@@ -1,16 +1,11 @@
 require([
-	'modules/jquery-mozu',
-	"modules/views-collections",
-	"modules/api", 
-	"modules/models-product",
-    "hyprlivecontext",
-    "modules/api",
-    "yotpo"
-], function( $, CollectionViewFactory, Api, ProductModel, HyprLiveContext, api, yotpo) { 
+    'modules/jquery-mozu',
+    "modules/views-collections" 
+], function( $, CollectionViewFactory) {  
     $(document).ready(function() {
         window.facetingViews = CollectionViewFactory.createFacetedCollectionViews({
             $body: $('[data-mz-category]'),
             template: "category-interior"
         });
     });  
-});  
+}); 
