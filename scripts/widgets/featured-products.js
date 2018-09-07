2ee70a6b35d9597ed8b5388bc9158f6a89bf63c4
utsahpaikraySteg <upaikray@stegtechnologies.com>
2018-09-07 12:43:22 +0530

    footer forms

--- scripts/widgets/featured-products.js
+++ scripts/widgets/featured-products.js
@@ -3,40 +3,41 @@ define([
     'bxslider',
     'lazyload'
 ],
-function ($, bxSlider, lazyload) { 
-     $("img.lazy").lazyload({
-        placeholder: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
-    });
-    $(document).on('click touchstart', 'a[class="bx-next"]', function () {
-        $("img.lazy").lazyload();
-    });
-    var slider;
-    var slide= {
-        productCarousel:function(){
-            var minSlides,
-                maxSlides,
-                slideWidth,
-                slideMargin,
-                windowWidth = $(window).width();
-            if (windowWidth >= 480 || windowWidth <= 767) {
-                minSlides = 2;
-                maxSlides = 2;
-                slideMargin = 10;
-                slideWidth = 333;
-            }
-             if (windowWidth < 480) {
+    function ($, bxSlider, lazyload) {
+        $("img.lazy").lazyload({
+            placeholder: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"
+        });
+        $(document).on('click touchstart', 'a[class="bx-next"]', function () {
+            $("img.lazy").lazyload();
+        });
+        var slider;
+        var slide = {
+            productCarousel: function () {
+                $(".mz-featured-products").prop("hidden", false);
+                var minSlides,
+                    maxSlides,
+                    slideWidth,
+                    slideMargin,
+                    windowWidth = $(window).width();
+                if (windowWidth >= 480 || windowWidth <= 767) {
+                    minSlides = 2;
+                    maxSlides = 2;
+                    slideMargin = 10;
+                    slideWidth = 333;
+                }
+                if (windowWidth < 480) {
                     minSlides = 1;
                     maxSlides = 1;
                     slideMargin = 10;
                     slideWidth = 333;
-            }
-            if (windowWidth > 767){
-                minSlides = 4;
-                maxSlides = 12;
-                slideWidth = 333;
-                slideMargin = 15;
-            }
-                slider=$(".mz-featured-products .mz-productlist-list").bxSlider({
+                }
+                if (windowWidth > 767) {
+                    minSlides = 4;
+                    maxSlides = 12;
+                    slideWidth = 333;
+                    slideMargin = 15;
+                }
+                slider = $(".mz-featured-products .mz-productlist-list").bxSlider({
                     auto: false,
                     speed: 600,
                     minSlides: minSlides,
@@ -48,17 +49,17 @@ function ($, bxSlider, lazyload) {
                     controls: true,
                     pager: false,
                     touchEnabled: true,
-                    preloadImages:"all",
+                    preloadImages: "all",
                     onSliderLoad: function () {
                         $(".slider").css("visibility", "visible");
                     }
                 });
                 window.slider = slider;
             }
-    };
-    slide.productCarousel();
-    $(window).resize(function () {
-        slider.destroySlider();
+        };
         slide.productCarousel();
+        $(window).resize(function () {
+            slider.destroySlider();
+            slide.productCarousel();
+        });
     });
-});
