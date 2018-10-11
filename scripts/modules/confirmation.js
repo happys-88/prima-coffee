d620cbbb4e6ea9ad02736cec06be6c7bf8002c07
singh-vsc <vsingh@stegtechnologies.com>
2018-10-11 17:24:11 +0530

    PRM-202 Changes

--- scripts/modules/confirmation.js
+++ scripts/modules/confirmation.js
@@ -105,10 +105,5 @@ require(["modules/jquery-mozu", "underscore", 'modules/api', "hyprlive", "module
         });
         dealsView.render();  
 
-        api.request("POST", "/commonRoute",{"requestFor":"yotpoSendOrder", "orderData":order}).then(function (response){
-            console.log("Success");
-        }, function(err) {
-            console.log("Error : "+JSON.stringify(err));
-        });
     });
 });
